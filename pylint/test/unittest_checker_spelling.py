bd5b47446b6fc2638ee98a86083060ddfc1b16dd
Kevin Locke <kevin@kevinlocke.name>
2016-12-02 15:26:17 -0700

    Use wordlist without system dictionary for testing
    This prevents the tests from depending on the enchant backends
    and dictionaries installed system-wide and makes the tests only depend
    on the wordlist included in the repository.
    
    This fixes the following error on my system:
    
        AssertionError: Expected messages did not match actual.
        Expected:
        Message(msg_id='wrong-spelling-in-comment', line=1, node=None, args=('coment', '# bad coment', '      ^^^^^^', "comet' or 'comment' or 'moment' or 'foment"))
        Got:
        Message(msg_id='wrong-spelling-in-comment', line=1, node=None, args=('coment', '# bad coment', '      ^^^^^^', "comet' or 'comment' or 'cement' or 'comest"))
    
    Signed-off-by: Kevin Locke <kevin@kevinlocke.name>

--- pylint/test/unittest_checker_spelling.py
+++ pylint/test/unittest_checker_spelling.py
@@ -5,6 +5,7 @@
 
 """Unittest for the spelling checker."""
 
+import os.path
 import unittest
 
 import astroid
@@ -14,40 +15,29 @@ from pylint.testutils import (
     CheckerTestCase, Message, set_config, tokenize_str,
 )
 
-# try to create enchant dictionary
 try:
     import enchant
 except ImportError:
     enchant = None
 
-spell_dict = None
-if enchant is not None:
-    try:
-        enchant.Dict("en_US")
-        spell_dict = "en_US"
-    except enchant.DictNotFoundError:
-        pass
+private_dict = os.path.join(os.path.dirname(__file__), 'data', 'wordlist.txt')
 
 
 class SpellingCheckerTest(CheckerTestCase):
     CHECKER_CLASS = spelling.SpellingChecker
 
-    @unittest.skipIf(spell_dict is None,
-                     "missing python-enchant package or missing "
-                     "spelling dictionaries")
-    @set_config(spelling_dict=spell_dict)
+    @unittest.skipIf(enchant is None, "missing python-enchant package")
+    @set_config(spelling_private_dict_file=private_dict)
     def test_check_bad_coment(self):
         with self.assertAddsMessages(
             Message('wrong-spelling-in-comment', line=1,
                     args=('coment', '# bad coment',
                           '      ^^^^^^',
-                          "comet' or 'comment' or 'moment' or 'foment"))):
+                          "comment' or 'moment"))):
             self.checker.process_tokens(tokenize_str("# bad coment"))
 
-    @unittest.skipIf(spell_dict is None,
-                     "missing python-enchant package or missing "
-                     "spelling dictionaries")
-    @set_config(spelling_dict=spell_dict)
+    @unittest.skipIf(enchant is None, "missing python-enchant package")
+    @set_config(spelling_private_dict_file=private_dict)
     def test_check_bad_docstring(self):
         stmt = astroid.extract_node(
             'def fff():\n   """bad coment"""\n   pass')
@@ -55,7 +45,7 @@ class SpellingCheckerTest(CheckerTestCase):
             Message('wrong-spelling-in-docstring', line=2,
                     args=('coment', 'bad coment',
                           '    ^^^^^^',
-                          "comet' or 'comment' or 'moment' or 'foment"))):
+                          "comment' or 'moment"))):
             self.checker.visit_functiondef(stmt)
 
         stmt = astroid.extract_node(
@@ -64,13 +54,11 @@ class SpellingCheckerTest(CheckerTestCase):
             Message('wrong-spelling-in-docstring', line=2,
                     args=('coment', 'bad coment',
                           '    ^^^^^^',
-                          "comet' or 'comment' or 'moment' or 'foment"))):
+                          "comment' or 'moment"))):
             self.checker.visit_classdef(stmt)
 
-    @unittest.skipIf(spell_dict is None,
-                     "missing python-enchant package or missing "
-                     "spelling dictionaries")
-    @set_config(spelling_dict=spell_dict)
+    @unittest.skipIf(enchant is None, "missing python-enchant package")
+    @set_config(spelling_private_dict_file=private_dict)
     def test_invalid_docstring_characters(self):
         stmt = astroid.extract_node(
             'def fff():\n   """test\\x00"""\n   pass')
